ba307099521740af217cb444c8a0658c3ee811be
TejasCodeBoss <tejasbrahme11@gmail.com>
2025-06-25 22:18:31 +0530

    Dashboard Changes

--- src/app/Pages/Dashboard/dashboard/dashboard.ts
+++ src/app/Pages/Dashboard/dashboard/dashboard.ts
@@ -16,7 +16,7 @@ export class Dashboard {
 doughnutChartLabels = ['Pending', 'In Progress', 'Completed'];
   doughnutChartData = {
     labels: this.doughnutChartLabels,
-    datasets: [{ data: [11, 5, 2], backgroundColor: ['#8e44ad', '#3498db', '#27ae60'] }]
+    datasets: [{ data: [7, 5, 2], backgroundColor: ['#8e44ad', '#3498db', '#27ae60'] }]
   };
   doughnutChartType: ChartType = 'doughnut';
 
@@ -30,4 +30,15 @@ doughnutChartLabels = ['Pending', 'In Progress', 'Completed'];
     }]
   };
   barChartType: ChartType = 'bar';
+
+  recentTasks = [
+  { name: 'Develop Product Review System', status: 'Pending', priority: 'Low', createdOn: '17th Mar 2025' },
+  { name: 'Build Feedback Form Module', status: 'Pending', priority: 'High', createdOn: '17th Mar 2025' },
+  { name: 'Implement Notification System', status: 'Pending', priority: 'Low', createdOn: '17th Mar 2025' },
+  { name: 'Migrate Database to MongoDB Atlas', status: 'Completed', priority: 'Medium', createdOn: '17th Mar 2025' },
+  { name: 'Develop Expense Tracker Module', status: 'Pending', priority: 'Low', createdOn: '17th Mar 2025' },
+  { name: 'Design Homepage Banner', status: 'Pending', priority: 'Medium', createdOn: '17th Mar 2025' },
+  { name: 'Write Technical Documentation', status: 'Pending', priority: 'Medium', createdOn: '17th Mar 2025' }
+];
+
 }
